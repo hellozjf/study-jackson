db87f9d6ea866b5eaa15d91b90867f4c71c7e859
hellozjf <908686171@qq.com>
2018-12-21 15:30:19 +0800

    init

---
+++ src/main/java/com/hellozjf/study/studyjackson/StudyJacksonApplication.java
@@ -0,0 +1,14 @@
+package com.hellozjf.study.studyjackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJacksonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJacksonApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/hellozjf/study/studyjackson/StudyJacksonApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hellozjf.study.studyjackson;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudyJacksonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
